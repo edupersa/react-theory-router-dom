eca2108d9562127561a8d516840e4a4069cc954e
Luis Peraza <leperazas@gmail.com>
2025-12-17 03:34:46 +0100

    Cambiado el style de los menus de navegacion.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import { useRef } from "react";
 import styles from "./Header.module.css";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
 
     const pathName = useLocation().pathname;
+
     console.log("Header component rendered");
 
     return (
@@ -11,7 +13,7 @@ const Header = () => {
             <h1>React Router DOM Example... </h1>
             <header className={styles.header}>
                 <nav className={styles.nav}>
-                    <Link className={pathName === "/" ? styles.active : ""} to="/"> Home </Link>
+                    <Link className={pathName ==="/" ? styles.active : ""} to="/"> Home </Link>
                     <Link className={pathName === "/about" ? styles.active : ""} to="/about"> About </Link>
                     <Link className={pathName === "/posts" ? styles.active : ""} to="/posts"> Post </Link>
                 </nav>
