91a0fab0135f471a3886f39e2bc55f4f748e95e3
Luis Peraza <leperazas@gmail.com>
2025-12-17 03:42:27 +0100

    Estilizado los menus.

--- src/components/PageHome/PageHome.tsx
+++ src/components/PageHome/PageHome.tsx
@@ -3,7 +3,9 @@ const PageHome = () => {
 
   console.log('PageHome rendered');
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1>Home Page</h1>
+    </>
   );
 } ;
 
--- src/components/PagePost/PagePost.tsx
+++ src/components/PagePost/PagePost.tsx
@@ -1,10 +1,10 @@
-
 const PagePost = () => {
-
-  console.log('PagePost rendered');
-  return (
-    <h1>Post Page</h1>
-  );
-} ;
+    console.log("PagePost rendered");
+    return (
+        <>
+          <h1>Post Page</h1>
+        </>
+    );
+};
 
 export default PagePost;
